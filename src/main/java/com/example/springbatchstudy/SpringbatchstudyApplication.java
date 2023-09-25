cb1153aea430e64aa93454d1cf90608791658c1b
aesperer <piyrw9754@gmail.com>
2023-09-26 08:49:01 +0900

    project setting

---
+++ src/main/java/com/example/springbatchstudy/SpringbatchstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatchstudy/SpringbatchstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
