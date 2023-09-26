a76f8be619719f521143ef07a78e2ece0eeda8ec
aesperer <piyrw9754@gmail.com>
2023-09-26 10:35:29 +0900

    add :: job scope

--- src/main/java/com/example/springbatchstudy/batch/TaskletJob.java
+++ src/main/java/com/example/springbatchstudy/batch/TaskletJob.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,6 +38,7 @@ public class TaskletJob {
     }
 
     @Bean
+    @JobScope
     public Step taskletJobStep2(@Value("#{jobParameters=[date]}") String date) {
         return stepBuilderFactory.get("taskletJob_step2")
                 .tasklet((a, b) -> {
