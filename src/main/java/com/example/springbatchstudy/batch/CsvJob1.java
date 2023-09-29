6b97dd1080ea6a4e006ae0b4f75d2642f9ec4acd
aesperer <piyrw9754@gmail.com>
2023-09-29 22:16:00 +0900

    csv job reader

---
+++ src/main/java/com/example/springbatchstudy/batch/CsvJob1.java
@@ -0,0 +1,46 @@
+package com.example.springbatchstudy.batch;
+
+import com.example.springbatchstudy.dto.TwoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class CsvJob1 {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final static int chunkSize = 5;
+
+    @Bean
+    public FlatFileItemReader<TwoDto> csvJob1FileReader() {
+        FlatFileItemReader<TwoDto> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new ClassPathResource("/sample/csvJob1_input.csv"));
+        flatFileItemReader.setLinesToSkip(1);
+
+        DefaultLineMapper<TwoDto> defaultLineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+        delimitedLineTokenizer.setNames("one", "two");
+        delimitedLineTokenizer.setDelimiter(":");
+
+        BeanWrapperFieldSetMapper<TwoDto> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        beanWrapperFieldSetMapper.setTargetType(TwoDto.class);
+
+        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
+        defaultLineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
+        flatFileItemReader.setLineMapper(defaultLineMapper);
+
+        return flatFileItemReader;
+    }
+}
